ad43cb88b52eaa5191a14bce253f2552e0a5ebd2
sc8ing <16541352+sc8ing@users.noreply.github.com>
2019-03-11 11:01:48 -0400

    combined terms seem to be working, still skeptical

--- bin/parser.ml
+++ bin/parser.ml
@@ -7,47 +7,41 @@ let rec term (tokens : Token.t list) : Lang.term * Token.t list =
   | Term e :: STIndicator :: tokens -> (SingleTerm e, tokens)
   | Term e :: tokens -> (Term e, tokens)
   | LPar :: tokens ->
-    (* combined terms first *)
-    (try
-      let ctTermPart tokens =
-        match tokens with
-        | Plus :: tokens ->
-          let (t, tokens) = term tokens in
-          (Plus, t, tokens)
-        | Minus :: tokens ->
-          let (t, tokens) = term tokens in
-          (Minus, t, tokens)
-        | _ -> failwith "not a combined term part"
-      in
-      let (typLeft, leftTerm, tokens) = ctTermPart tokens in
-      let (typRight, rightTerm, tokens) = ctTermPart tokens in
-      match tokens with
-      | RPar :: tokens ->
-        (match (typLeft, typRight) with
-         | (Plus, Plus) ->
-           let _ = Printf.printf "made intersection\n" in
-           (Intersection (leftTerm, rightTerm), tokens)
-         | (Plus, Minus) -> (Without (leftTerm, rightTerm), tokens)
-         | (Minus, Plus) -> (Union (leftTerm, rightTerm), tokens)
-         | (Minus, Minus) -> (Nor (leftTerm, rightTerm), tokens)
-         | _ -> failwith "should not be possible")
-      | _ -> failwith ("combined term missing closing " ^ (Token.toString Token.RPar))
-    (* then try normal terms *)
-    with Failure _ ->
-      let _ = Printf.printf "failed to make ct, tokens: %s\n" (Token.toStrings tokens) in
-      let (innerT, tokens) = innerTerm tokens in
-      (match tokens with
-       | RPar :: tokens -> (innerT, tokens)
-       | _ -> failwith (Printf.sprintf "missing closing '%s'" (Token.toString Token.RPar))))
+    let (inner, tokens) = innerTerm tokens in
+    (match tokens with
+     | RPar :: tokens -> (inner, tokens)
+     | _ -> failwith "missing rpar")
   | _ -> failwith "invalid term form"
 
-and innerTerm = function
+and innerTerm tokens =
+  try (* to make combined term *)
+    let ctTermPart tokens = 
+      match tokens with
+      | Plus :: tokens -> (Plus, term tokens)
+      | Minus :: tokens -> (Minus, term tokens)
+      | _ -> failwith "not cttermpart"
+    in
+    let (typLeft, (leftT, tokens)) = ctTermPart tokens in
+    let (typRight, (rightT, tokens)) = ctTermPart tokens in
+    (match (typLeft, typRight) with
+     | (Plus, Plus) -> (Intersection (leftT, rightT), tokens)
+     | (Plus, Minus) -> (Without (leftT, rightT), tokens)
+     | (Minus, Plus) -> (Union (leftT, rightT), tokens)
+     | (Minus, Minus) -> (Nor (leftT, rightT), tokens)
+     | _ -> failwith "ctTermPart shouldn't return anything other than Plus or Minus")
+  with Failure _ -> (* normal term *)
+  match tokens with
   | Minus :: Term e :: STIndicator :: tokens -> (Neg (SingleTerm e), tokens)
   | Minus :: Term e :: tokens -> (Neg (Term e), tokens)
   | Minus :: LPar :: tokens ->
-    let (innerT, tokens) = term tokens in
+    let (innerT, tokens) = innerTerm tokens in
     (match tokens with
      | RPar :: tokens -> (Neg innerT, tokens)
+     | _ -> failwith (Printf.sprintf "missing closing '%s'" (Token.toString Token.RPar)))
+  | LPar :: tokens ->
+    let (innerT, tokens) = innerTerm tokens in
+    (match tokens with
+     | RPar :: tokens -> (innerT, tokens)
      | _ -> failwith (Printf.sprintf "missing closing '%s'" (Token.toString Token.RPar)))
   | tokens -> term tokens
 
@@ -65,16 +59,20 @@ let rec statement tokens =
   | Minus :: LPar :: tokens ->
     (* statement case *)
     (try
+       let _ = Printf.printf "ambiguous case, trying to make statement from %s\n" (Token.toStrings tokens) in
        let (innerS, tokens) = statement tokens in
        (match tokens with
-        | RPar :: [] -> (Neg innerS, tokens) (* no leftover tokens excludes case of combined term *)
-        | _ -> failwith (Printf.sprintf "missing closing '%s'" (Token.toString Token.RPar)))
+        | RPar :: [] ->
+          let _ = Printf.printf "successfully made statement from ambiguous case\n" in
+          (Neg innerS, tokens) (* no leftover tokens excludes case of combined term *)
+        | _ ->
+          let _ = Printf.printf "failed to make statement from ambiguous case, leftOvers = %s\n" (Token.toStrings tokens) in
+          failwith (Printf.sprintf "missing closing '%s'" (Token.toString Token.RPar)))
      with Failure _ ->
-     (* term case *)
-     try
-       let _ = Printf.printf "failed to make statement\n" in
+       (* term case *)
+       let _ = Printf.printf "failed to make statement, trying to make term from %s\n" (Token.toStrings tokens) in
        let (sub, tokens) = term (LPar :: tokens) in
-       let _ = Printf.printf "statement got subject, tokens: %s\n" (Token.toStrings tokens) in
+       let _ = Printf.printf "successfully made term as subject, tokens: %s\n" (Token.toStrings tokens) in
        (match tokens with
         | Plus :: tokens ->
           let (pred, tokens) = term tokens in
@@ -83,7 +81,7 @@ let rec statement tokens =
           let (pred, tokens) = term tokens in
           (Statement { quan = Universal; qual = Negative; sub; pred }, tokens)
         | _ -> failwith "invalid statement")
-     with Failure _ -> failwith "bad statement")
+    )
   | Plus :: tokens ->
     let (sub, tokens) = term tokens in
     (match tokens with
@@ -94,7 +92,7 @@ let rec statement tokens =
        let (pred, tokens) = term tokens in
        (Statement { quan = Particular; qual = Negative; sub; pred }, tokens)
      | _ -> failwith "invalid statement")
-   | Minus :: tokens ->
+  | Minus :: tokens ->
     let (sub, tokens) = term tokens in
     (match tokens with
      | Plus :: tokens ->
@@ -104,9 +102,9 @@ let rec statement tokens =
        let (pred, tokens) = term tokens in
        (Statement { quan = Universal; qual = Negative; sub; pred }, tokens)
      | _ -> failwith "invalid statement")
-   | _ -> failwith "invalid statement"
+  | _ -> failwith "invalid statement"
 
-let parse tokens =
+and parse tokens =
   let (result, leftOver) = statement tokens in
   match leftOver with
   | [] -> result
